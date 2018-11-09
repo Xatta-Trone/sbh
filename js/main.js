5af766e6e914453e932b45ba502ace1dd213ea27
Monzurul Islam <monzurul.ce.buet@gmail.com>
2018-11-09 15:09:19 +0600

    new section

--- js/main.js
+++ js/main.js
@@ -2,7 +2,14 @@
 	"use strict";
 
     jQuery(document).ready(function($){
-
+    	    					//year/month/day
+    		$('#clock').countdown('2019/02/20 12:00:00').on('update.countdown', function(event) {
+    	  		var $this = $(this).html(event.strftime(''
+    					+ '<div class="wrapper"><span class="time">%-D</span><br>day%!D</div><span class="slash">/</span>'
+    					+ '<div class="wrapper"><span class="time">%H</span><br>hours</div><span class="slash">/</span>'
+    					+ '<div class="wrapper"><span class="time">%M</span><br>minutes</div><span class="slash">/</span>'
+    					+ '<div class="wrapper"><span class="time">%S</span><br>seconds</div>'));	
+    		});
 
 
 
