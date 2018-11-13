54a11bbf0a2f2ee17d8f200e2c41732973782677
Monzurul Islam <monzurul.ce.buet@gmail.com>
2018-11-13 23:26:41 +0600

    home page done

--- js/main.js
+++ js/main.js
@@ -1,25 +1,6 @@
-(function ($) {
-	"use strict";
-
-    jQuery(document).ready(function($){
-    	    					//year/month/day
-    		$('#clock').countdown('2019/02/20 12:00:00').on('update.countdown', function(event) {
-    	  		var $this = $(this).html(event.strftime(''
-    					+ '<div class="wrapper"><span class="time">%-D</span><br>day%!D</div><span class="slash">/</span>'
-    					+ '<div class="wrapper"><span class="time">%H</span><br>hours</div><span class="slash">/</span>'
-    					+ '<div class="wrapper"><span class="time">%M</span><br>minutes</div><span class="slash">/</span>'
-    					+ '<div class="wrapper"><span class="time">%S</span><br>seconds</div>'));	
-    		});
+$(document).ready(function(){
 
 
 
-    });
+});
 
-
-    jQuery(window).load(function(){
-
-        
-    });
-
-
-}(jQuery));	
